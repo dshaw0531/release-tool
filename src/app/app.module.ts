0b4c73b1e073f1a577697127307d045505964d63
Deanna Shaw <dshaw@reliaslearning.com>
2018-04-04 19:45:26 -0400

    Add step1 component and beginnings of accordion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { Step1Component } from './step1/step1.component';
 
+const appRoutes: Routes = [
+  { path: 'step1', component: Step1Component },
+  { path: '**', redirectTo: 'step1', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Step1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    ),
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
