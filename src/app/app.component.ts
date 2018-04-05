37d2014e3354498728bfc2e05648a7284bb475ed
Deanna Shaw <dshaw@reliaslearning.com>
2018-04-05 13:45:39 -0400

    Start release service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RLMS Release Tool';
+  items: FirebaseListObservable<any[]>;
 }
