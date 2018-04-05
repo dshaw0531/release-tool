3d1071a8fce3a40b731023b83a31aa964bf14070
Karch McCoy <kmccoy@reliaslearning.com>
2018-04-05 16:38:44 -0400

    Moving over to the passed in values

--- src/app/generate-word-doc/generate-word-doc.component.ts
+++ src/app/generate-word-doc/generate-word-doc.component.ts
@@ -25,6 +25,31 @@ export class GenerateWordDocComponent implements OnInit {
   generateCN: boolean;
   generateUS: boolean;
 
+  releaseName: string;
+  releaseOwner: string;
+  releaseOwnerContactInfo: string;
+  chmTicket: string;
+
+  legacyReleaseNumber: string;
+  angularReleaseNumber: string;
+  apiReleaseNumber: string;
+  internalToolsReleaseNumber: string;
+  enrollmentsServiceReleaseNumber: string;
+  reportingServiceReleaseNumber: string;
+  learningServiceReleaseNumber: string;
+
+  lmsMigrationScript: string;
+  lmsProcChangesScript: string;
+  lmsLogMigrationScript: string;
+  lmsLogProcChangesScript: string;
+  lmsReportSchemaScript: string;
+  lmsReportProcChangesScript: string;
+  lmsAuditChangesScript: string;
+  lmsTriggersChangesScript: string;
+
+  newUtilityName: string;
+  newUtilityScheduledTime: string;
+
   constructor() { }
 
   ngOnInit() {
@@ -51,6 +76,32 @@ export class GenerateWordDocComponent implements OnInit {
     this.UK = 1;
     this.CN = 2;
     this.US = 3;
+
+    // Test Values
+    this.releaseName = '2018.04.03';
+    this.releaseOwner = 'Karch McCoy';
+    this.releaseOwnerContactInfo = '(314)620-4774, kmccoy@relias.com';
+    this.chmTicket = 'CHM-273';
+
+    this.legacyReleaseNumber = 'release-266';
+    this.apiReleaseNumber = 'release-40';
+    this.angularReleaseNumber = 'release-29';
+    this.internalToolsReleaseNumber = 'release-50';
+    this.enrollmentsServiceReleaseNumber = 'release-5';
+    this.reportingServiceReleaseNumber = 'release-11';
+    this.learningServiceReleaseNumber = 'release-4';
+
+    this.lmsMigrationScript = 'LMS Migrations.sql';
+    this.lmsLogMigrationScript = 'LMSLog Migrations.sql';
+    this.lmsAuditChangesScript = 'LMSAudit Changes.sql';
+    this.lmsTriggersChangesScript = 'LMSTriggers Changes.sql';
+    this.lmsProcChangesScript = 'LMS Schema Compare.sql';
+    this.lmsLogProcChangesScript = 'LMSLog Schema Compare.sql';
+    this.lmsReportSchemaScript = 'LMSReport Schema.sql';
+    this.lmsReportProcChangesScript = 'LMSReport Schema Compare.sql';
+
+    this.newUtilityName = 'New Utility Test';
+    this.newUtilityScheduledTime = '3:30pm - Tuesdays';
   }
 
   getServers(servers: string[]): string {
